bcfe404164d43674c46645750a765995d6b1f3fc
João Marcelo Vieira <joaonininho.twt@gmail.com>
2022-07-23 10:59:42 -0300

    fix: solving build problem

---
+++ .next/server/pages/api/health.js
@@ -0,0 +1,34 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 837;
+exports.ids = [837];
+exports.modules = {
+
+/***/ 854:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ handler)
+/* harmony export */ });
+function handler(_req, res) {
+    res.json({
+        status: "ok"
+    });
+};
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-api-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(854));
+module.exports = __webpack_exports__;
+
+})();
